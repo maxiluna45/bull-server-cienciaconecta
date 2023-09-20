1e21e7acc305743639f751a3043878473337fe45
Alejo Dominguez <alejomariodominguez@gmail.com>
2023-09-20 11:20:15 -0300

    nueva cola y funciones

--- queues.js
+++ queues.js
@@ -5,10 +5,18 @@ import  { emailWorker } from "./workers/email.js"
 const email = new Queue("email", { redis: config.redis });
 email.process((job, done) => emailWorker(job, done));
 
+const file = new Queue("file",{redis: config.redis});
+file.process((job, done) => fileWorker(job,done));
+
 export const queues = [
     {
       name: "email",
       hostId: "Email Queue Manager",
       redis: config.redis,
     },
+    {
+      name:"file",
+      hostId:"File Queue Manager",
+      redis: config.redis,
+    }
 ];
---
+++ workers/file.js
@@ -0,0 +1,58 @@
+import { drive } from "googleapis";
+import { shareFolderWithPersonalAccount , createFolder , sendFileToDrive} from "../services/helpers-drive.js"
+
+export const fileWorker = async(job , done) => {
+    try {
+        const { proyecto , files  } = job.data;
+        const name_folder = proyecto.titulo;
+        console.log('test archivos' , files);
+        console.log(proyecto);
+        //creo la nueva carpeta
+        const id_folder_new = await createFolder(name_folder, drive);
+
+      //seteo el campo del proyecto "id_carpeta_drive" con el "id" de la carpeta creada
+        proyecto.id_carpeta_drive = id_folder_new;
+
+      // Compartir la carpeta creada en paralelo
+      const email_ciencia_conecta = "cienciaconecta.utn@gmail.com";
+      await Promise.all([
+        shareFolderWithPersonalAccount(
+          id_folder_new,
+          email_ciencia_conecta,
+          drive,
+          "writer"
+        ),
+      ]);
+
+      const uploadPromises = [];
+
+      // Subir los archivos en paralelo
+      uploadPromises.push(
+        sendFileToDrive(files.registroPedagogicopdf, id_folder_new, drive)
+      );
+      uploadPromises.push(
+        sendFileToDrive(files.carpetaCampo, id_folder_new, drive)
+      );
+      uploadPromises.push(
+        sendFileToDrive(files.informeTrabajo, id_folder_new, drive)
+      );
+      uploadPromises.push(
+        sendFileToDrive(files.autorizacionImagen, id_folder_new, drive)
+      );
+
+      const [id_archivo_pdf, id_archivo_pdf_campo, id_archivo_informeTrabajo, id_archivo_autorizacionImagen] =
+        await Promise.all(uploadPromises);
+
+      proyecto.registroPedagogico = `https://drive.google.com/file/d/${id_archivo_pdf}/preview`;
+      proyecto.carpetaCampo = `https://drive.google.com/file/d/${id_archivo_pdf_campo}/preview`;
+      proyecto.informeTrabajo = `https://drive.google.com/file/d/${id_archivo_informeTrabajo}/preview`;
+      proyecto.autorizacionImagen = `https://drive.google.com/file/d/${id_archivo_autorizacionImagen}/preview`;
+
+
+        done();
+    } catch (error) {
+        console.error(error);
+        done(error);
+
+    }
+}
