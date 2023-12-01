1891468dec82f936f943e0929b7c685aac3668ee
maxiluna45 <maximilianoluna3645@gmail.com>
2023-12-01 19:51:13 -0300

    [CNC-163] Notificaciones de carga de archivos de proyecto y CV

--- helpers/generarNotificacion.js
+++ helpers/generarNotificacion.js
@@ -29,30 +29,22 @@ export const generarNotificacion = async (usuario, mensaje) => {
 
 
 export const tipo_notificacion = {
-    //
     promocion: (titulo, instancia) => `Tu proyecto '${titulo}' ha sido promovido a la instancia ${instancia}. ¡Felicidades!.`,
-    //
     subir_documentos: (titulo) => `Tu proyecto '${titulo}' debe ser actualizado con los documentos correspondientes. Por favor, editá tu proyecto para actualizarlo.`,
-    //
     no_promocion: (titulo, instancia) => `Tu proyecto '${titulo}' no ha sido promovido a la instancia ${instancia}. ¡Suerte para el próximo año!.`,
-    //
     inicio_evaluacion: `La evaluación teórica de proyectos en la instancia Regional ha comenzado. Ya puedes comenzar a evaluar tus proyectos asignados.`,
-    //
     inicio_exposicion: (instancia) =>`La evaluación de exposición de proyectos en la instancia ${instancia} ha comenzado. Ya puedes comenzar a evaluar tus proyectos asignados.`,
-    //
     cinco_dias_evaluacion: (tipo, instancia) => `Sólo te quedan 5 días para completar la evaluación ${tipo} de tus proyectos en instancia ${instancia}.`,
-    //
     un_dia_evaluacion: (tipo, instancia) => `Sólo te queda 1 día para completar la evaluación ${tipo} de tus proyectos en instancia ${instancia}.`,
-    
     cinco_dias_asignacion: (cantidad) =>`Sólo te quedan 5 días para asignar evaluadores a los proyectos de tu sede. Te queda/n ${cantidad} proyecto/s por asignar.`,
-   
     un_dia_asignacion: (cantidad) =>`Sólo te queda 1 día para asignar evaluadores a los proyectos de tu sede. Te queda/n ${cantidad} proyecto/s por asignar.`,
-    //
     inicio_asignacion: "Ya podés comenzar a asignar evaluadores a los proyectos de tu sede.",
-    //
     actualizacion_exitosa: `La actualización de Establecimientos Educativos ha finalizado exitosamente`,
-    //
     creacion_exitosa: `Se han cargado todos los Establecimientos con éxito. No existían Establecimientos Educativos almacenados previamente.`,
-    //
     actualizacion_fallida: `Ha fallado la actualización de Establecimientos Educativos`,
+    carga_exitosa_proyecto: (proyecto) => `La carga de los documentos del proyecto ${proyecto} ha finalizado exitosamente`,
+    carga_fallida_proyecto: (proyecto) => `La carga de los documentos del proyecto ${proyecto} ha fallado. Por favor, intentá nuevamente más tarde.`,
+    carga_exitosa_cv: `La carga de tu CV ha finalizado exitosamente.`,
+    carga_fallida_cv: `La carga de tu CV ha fallado. Por favor, intentá postularte como evaluador nuevamente.`,
+
 }
--- workers/file.js
+++ workers/file.js
@@ -9,12 +9,14 @@ import {
 import { Proyecto } from "../models/Proyecto.js";
 import { Evaluador } from "../models/Evaluador.js";
 import { Docente } from "../models/Docente.js";
+import { generarNotificacion, tipo_notificacion } from "../helpers/generarNotificacion.js";
 
-export const UploadFiles = async (id_proyecto, files , name_files) => {
+export const UploadFiles = async (uid, id_proyecto, files , name_files) => {
+  
+  const proyecto = await Proyecto.findById(id_proyecto);
+  
   try {
-    console.log(id_proyecto);
-    const proyecto = await Proyecto.findById(id_proyecto);
-    console.log(proyecto);
+
     const name_folder = proyecto.titulo;
     //creo la nueva carpeta
     const id_folder_new = await createFolder(name_folder, drive);
@@ -71,17 +73,27 @@ export const UploadFiles = async (id_proyecto, files , name_files) => {
       proyecto.nameRegistroPedagogicopdf = files.registroPedagogicopdf.originalFilename;
       proyecto.nameInformeTrabajo = files.informeTrabajo.originalFilename;
       await proyecto.save();
+      await generarNotificacion(uid, tipo_notificacion.carga_exitosa_proyecto(proyecto.titulo))
     } else {
-      throw new Error(`NO SE PUDO CARGAR LOS DOCUMENTOS.`);
+      throw new Error(`No se han podido cargar los documentos de proyecto`);
     }
+
   } catch (error) {
+
     console.error(error);
+    if(proyecto){
+      await generarNotificacion(uid, tipo_notificacion.carga_fallida_proyecto(proyecto.titulo))
+    }
+
   }
 };
 
-export const UpdateFiles = async (id , files , name_files) => {
+export const UpdateFiles = async (uid, id , files , name_files) => {
+  
+  const proyecto = await Proyecto.findById(id);
+  
   try {
-    const proyecto = await Proyecto.findById(id);
+
     const id_folder = proyecto.id_carpeta_drive;
     let id_archivo_pdf = null;
     let id_carpeta_campo = null;
@@ -157,197 +169,226 @@ export const UpdateFiles = async (id , files , name_files) => {
 
       
       await proyecto.save();
+      await generarNotificacion(uid, tipo_notificacion.carga_exitosa_proyecto(proyecto.titulo))
     } else {
+      await generarNotificacion(uid, tipo_notificacion.carga_fallida_proyecto(proyecto.titulo))
       throw new Error(`NO SE PUDO CARGAR LOS DOCUMENTOS.`);
     }
+
   } catch (error) {
+
     console.error(error);
+
+    if(proyecto){
+      await generarNotificacion(uid, tipo_notificacion.carga_fallida_proyecto(proyecto.titulo))
+    }
+
   }
 }
 
-export const fileWorker = async (job, done) => {
-  try {
-    const { id_proyecto, files } = job.data;
-    //busco el proyecto que pertenece ese id
-    const proyecto = await Proyecto.findById(id_proyecto);
+// export const fileWorker = async (job, done) => {
+//   try {
+//     const { id_proyecto, files } = job.data;
+//     //busco el proyecto que pertenece ese id
+//     const proyecto = await Proyecto.findById(id_proyecto);
 
-    const name_folder = proyecto.titulo;
-    console.log("test archivos", files);
-    console.log(proyecto);
-    //creo la nueva carpeta
-    const id_folder_new = await createFolder(name_folder, drive);
+//     const name_folder = proyecto.titulo;
+//     console.log("test archivos", files);
+//     console.log(proyecto);
+//     //creo la nueva carpeta
+//     const id_folder_new = await createFolder(name_folder, drive);
 
-    //seteo el campo del proyecto "id_carpeta_drive" con el "id" de la carpeta creada
-    proyecto.id_carpeta_drive = id_folder_new;
-    console.log(proyecto.id_carpeta_drive);
-    // Compartir la carpeta creada en paralelo
-    const email_ciencia_conecta = "cienciaconecta.utn@gmail.com";
-    await Promise.all([
-      shareFolderWithPersonalAccount(
-        id_folder_new,
-        email_ciencia_conecta,
-        drive,
-        "writer"
-      ),
-    ]);
+//     //seteo el campo del proyecto "id_carpeta_drive" con el "id" de la carpeta creada
+//     proyecto.id_carpeta_drive = id_folder_new;
+//     console.log(proyecto.id_carpeta_drive);
+//     // Compartir la carpeta creada en paralelo
+//     const email_ciencia_conecta = "cienciaconecta.utn@gmail.com";
+//     await Promise.all([
+//       shareFolderWithPersonalAccount(
+//         id_folder_new,
+//         email_ciencia_conecta,
+//         drive,
+//         "writer"
+//       ),
+//     ]);
 
-    const uploadPromises = [];
+//     const uploadPromises = [];
 
-    // Subir los archivos en paralelo
-    uploadPromises.push(
-      sendFileToDrive(files.registroPedagogicopdf, id_folder_new, drive)
-    );
-    uploadPromises.push(
-      sendFileToDrive(files.carpetaCampo, id_folder_new, drive)
-    );
-    uploadPromises.push(
-      sendFileToDrive(files.informeTrabajo, id_folder_new, drive)
-    );
-    uploadPromises.push(
-      sendFileToDrive(files.autorizacionImagen, id_folder_new, drive)
-    );
+//     // Subir los archivos en paralelo
+//     uploadPromises.push(
+//       sendFileToDrive(files.registroPedagogicopdf, id_folder_new, drive)
+//     );
+//     uploadPromises.push(
+//       sendFileToDrive(files.carpetaCampo, id_folder_new, drive)
+//     );
+//     uploadPromises.push(
+//       sendFileToDrive(files.informeTrabajo, id_folder_new, drive)
+//     );
+//     uploadPromises.push(
+//       sendFileToDrive(files.autorizacionImagen, id_folder_new, drive)
+//     );
 
-    const [
-      id_archivo_pdf,
-      id_archivo_pdf_campo,
-      id_archivo_informeTrabajo,
-      id_archivo_autorizacionImagen,
-    ] = await Promise.all(uploadPromises);
+//     const [
+//       id_archivo_pdf,
+//       id_archivo_pdf_campo,
+//       id_archivo_informeTrabajo,
+//       id_archivo_autorizacionImagen,
+//     ] = await Promise.all(uploadPromises);
 
-    proyecto.registroPedagogico = `https://drive.google.com/file/d/${id_archivo_pdf}/preview`;
-    proyecto.carpetaCampo = `https://drive.google.com/file/d/${id_archivo_pdf_campo}/preview`;
-    proyecto.informeTrabajo = `https://drive.google.com/file/d/${id_archivo_informeTrabajo}/preview`;
-    proyecto.autorizacionImagen = `https://drive.google.com/file/d/${id_archivo_autorizacionImagen}/preview`;
+//     proyecto.registroPedagogico = `https://drive.google.com/file/d/${id_archivo_pdf}/preview`;
+//     proyecto.carpetaCampo = `https://drive.google.com/file/d/${id_archivo_pdf_campo}/preview`;
+//     proyecto.informeTrabajo = `https://drive.google.com/file/d/${id_archivo_informeTrabajo}/preview`;
+//     proyecto.autorizacionImagen = `https://drive.google.com/file/d/${id_archivo_autorizacionImagen}/preview`;
 
-    if (
-      id_archivo_pdf &&
-      id_archivo_pdf_campo &&
-      id_archivo_informeTrabajo &&
-      id_archivo_autorizacionImagen
-    ) {
-      await proyecto.save();
-      done();
-    } else {
-      done();
-    }
-  } catch (error) {
-    console.error(error);
-    done(error);
-  }
-};
+//     if (
+//       id_archivo_pdf &&
+//       id_archivo_pdf_campo &&
+//       id_archivo_informeTrabajo &&
+//       id_archivo_autorizacionImagen
+//     ) {
+//       await proyecto.save();
+//       done();
+//     } else {
+//       done();
+//     }
+//   } catch (error) {
+//     console.error(error);
+//     done(error);
+//   }
+// };
 
-export const ActualizarDocumentsWotker = async (job, done) => {
-  try {
-    const { id, files } = job.data;
-    const proyecto = await Proyecto.findById(id);
-    console.log(proyecto.id_carpeta_drive);
-    const id_folder = proyecto.id_carpeta_drive;
-    let id_archivo_pdf = null;
-    let id_carpeta_campo = null;
-    let id_informe_trabajo = null;
-    let id_autorizacion_imagen = null;
+// export const ActualizarDocumentsWotker = async (job, done) => {
+//   try {
+//     const { id, files } = job.data;
+//     const proyecto = await Proyecto.findById(id);
+//     console.log(proyecto.id_carpeta_drive);
+//     const id_folder = proyecto.id_carpeta_drive;
+//     let id_archivo_pdf = null;
+//     let id_carpeta_campo = null;
+//     let id_informe_trabajo = null;
+//     let id_autorizacion_imagen = null;
 
-    const processFile = async (file, existingFile) => {
-      if (file) {
-        if (existingFile) {
-          const id = await getIdByUrl(existingFile);
-          if (id) {
-            const deleteResult = await deleteFile(id, drive);
-            if (deleteResult) {
-              return await sendFileToDrive(file, id_folder, drive);
-            }
-          }
-        } else {
-          return await sendFileToDrive(file, id_folder, drive);
-        }
-      }
-      return null;
-    };
+//     const processFile = async (file, existingFile) => {
+//       if (file) {
+//         if (existingFile) {
+//           const id = await getIdByUrl(existingFile);
+//           if (id) {
+//             const deleteResult = await deleteFile(id, drive);
+//             if (deleteResult) {
+//               return await sendFileToDrive(file, id_folder, drive);
+//             }
+//           }
+//         } else {
+//           return await sendFileToDrive(file, id_folder, drive);
+//         }
+//       }
+//       return null;
+//     };
 
-    id_archivo_pdf = await processFile(
-      files.registroPedagogicopdf,
-      proyecto.registroPedagogico
-    );
+//     id_archivo_pdf = await processFile(
+//       files.registroPedagogicopdf,
+//       proyecto.registroPedagogico
+//     );
 
-    id_carpeta_campo = await processFile(
-      files.carpetaCampo,
-      proyecto.carpetaCampo
-    );
+//     id_carpeta_campo = await processFile(
+//       files.carpetaCampo,
+//       proyecto.carpetaCampo
+//     );
 
-    id_informe_trabajo = await processFile(
-      files.informeTrabajo,
-      proyecto.informeTrabajo
-    );
+//     id_informe_trabajo = await processFile(
+//       files.informeTrabajo,
+//       proyecto.informeTrabajo
+//     );
 
-    id_autorizacion_imagen = await processFile(
-      files.autorizacionImagen,
-      proyecto.autorizacionImagen
-    );
+//     id_autorizacion_imagen = await processFile(
+//       files.autorizacionImagen,
+//       proyecto.autorizacionImagen
+//     );
 
-    if (
-      id_archivo_pdf ||
-      id_carpeta_campo ||
-      id_autorizacion_imagen ||
-      id_informe_trabajo
-    ) {
-      proyecto.registroPedagogico = id_archivo_pdf
-        ? `https://drive.google.com/file/d/${id_archivo_pdf}/preview`
-        : proyecto.registroPedagogico;
-      proyecto.carpetaCampo = id_carpeta_campo
-        ? `https://drive.google.com/file/d/${id_carpeta_campo}/preview`
-        : proyecto.carpetaCampo;
-      proyecto.informeTrabajo = id_informe_trabajo
-        ? `https://drive.google.com/file/d/${id_informe_trabajo}/preview`
-        : proyecto.informeTrabajo;
-      proyecto.autorizacionImagen = id_autorizacion_imagen
-        ? `https://drive.google.com/file/d/${id_autorizacion_imagen}/preview`
-        : proyecto.autorizacionImagen;
-      await proyecto.save();
-      done();
-    } else {
-      done();
-    }
-  } catch (error) {
-    console.error(error);
-    done(error);
-  }
-};
+//     if (
+//       id_archivo_pdf ||
+//       id_carpeta_campo ||
+//       id_autorizacion_imagen ||
+//       id_informe_trabajo
+//     ) {
+//       proyecto.registroPedagogico = id_archivo_pdf
+//         ? `https://drive.google.com/file/d/${id_archivo_pdf}/preview`
+//         : proyecto.registroPedagogico;
+//       proyecto.carpetaCampo = id_carpeta_campo
+//         ? `https://drive.google.com/file/d/${id_carpeta_campo}/preview`
+//         : proyecto.carpetaCampo;
+//       proyecto.informeTrabajo = id_informe_trabajo
+//         ? `https://drive.google.com/file/d/${id_informe_trabajo}/preview`
+//         : proyecto.informeTrabajo;
+//       proyecto.autorizacionImagen = id_autorizacion_imagen
+//         ? `https://drive.google.com/file/d/${id_autorizacion_imagen}/preview`
+//         : proyecto.autorizacionImagen;
+//       await proyecto.save();
+//       done();
+//     } else {
+//       done();
+//     }
+//   } catch (error) {
+//     console.error(error);
+//     done(error);
+//   }
+// };
+
+
 
 export const UploadCv = async (job, done) => {
-  try {
-    const { uid, files } = job.data;
+  
+  const { uid, files } = job.data;
 
-    const _docente = await Docente.findOne({ usuario: uid });
-    const evaluador = await Evaluador.findOne({ idDocente: _docente.id });
+  const _docente = await Docente.findOne({ usuario: uid });
+  const evaluador = await Evaluador.findOne({ idDocente: _docente.id });
+  
+  try {
 
     const name_folder = _docente.cuil;
     //creo la nueva carpeta
     const id_folder_new = await createFolder(name_folder, drive);
-    console.log(name_folder);
-    console.log("carpeta", id_folder_new);
+
     if (id_folder_new) {
+
       evaluador.id_carpeta_cv = id_folder_new;
+
       // Compartir la carpeta creada en paralelo
       const email_ciencia_conecta = "cienciaconecta.utn@gmail.com";
+
       await shareFolderWithPersonalAccount(
         id_folder_new,
         email_ciencia_conecta,
         drive,
         "writer"
       );
+
       const id_cv = await sendFileToDrive(files.cv, id_folder_new, drive);
+
       if (id_cv) {
         evaluador.CV = `${id_cv}`;
         await evaluador.save();
+        await generarNotificacion(uid, tipo_notificacion.carga_exitosa_cv)
         done();
+
       } else {
-        done(error);
+        throw new Error("No se ha podido cargar el archivo en Drive");
       }
+
     } else {
-      done(error);
+      throw new Error("No se ha podido crear la carpeta en Drive");
     }
+
   } catch (error) {
+
     console.error(error);
-    done();
+
+    if (_docente && evaluador) {
+      await generarNotificacion(uid, tipo_notificacion.carga_fallida_cv);
+      await evaluador.deleteOne();
+    }
+
+    done(error);
+
   }
 };
